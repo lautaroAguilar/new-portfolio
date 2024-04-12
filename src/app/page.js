a4fa48afb78f69da8ef87740d1d68e1413a67526
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-04-12 16:50:37 -0300

    create background div with position absolute

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,10 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      
+      <div className={styles.background}></div>
+      <header className={styles.header}>
+        <h1>Hello i'm Lautaro</h1>
+      </header>
     </main>
   );
 }
