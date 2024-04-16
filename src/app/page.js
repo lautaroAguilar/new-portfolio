c19036742f9c2e557f6bdc0edb6be0bc1ed35729
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-04-15 22:30:52 -0300

    add animation with glsl

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "../components/Header/Header";
-
+import BackgroundAnimation from "@/components/BackgroundAnimation/BackgroundAnimation";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.background}></div>
+      <BackgroundAnimation/>
       <Header />
       <section className={styles.section}>
         <h1>otros contenidos</h1>
