b72e756c159d321133c9cd1bfcdec76364215d82
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-04-30 21:22:30 -0300

    add section animation

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,33 +1,60 @@
 "use client";
 import { useAppContext } from "@/context/appContext";
 import styles from "./Section.module.css";
-import { playfair } from "../Header/Header";
+import { useState, useEffect } from "react";
 export default function Section() {
-  const { sectionState } = useAppContext();
-
-  return (
+  const { sectionState, isExiting } = useAppContext();
+  const [animationClass, setAnimationClass] = useState("");
+  const [currentSection, setCurrentSection] = useState(null);
+  
+  /* falta ajustar para que en la primer carga no me haga la animacion */
+  useEffect(() => {
+    if (isExiting) {
+        setAnimationClass(styles.fade_out_left);
+        setTimeout(() => {
+            setCurrentSection(null); // Limpia la sección durante la animación de salida
+        }, 500);
+    } else if (sectionState) {
+        const timer = setTimeout(() => {
+            setCurrentSection(sectionState);
+            setAnimationClass(styles.fade_in_right);
+        }, 100); // Un pequeño retraso antes de aplicar la animación de entrada
+        return () => clearTimeout(timer);
+    }
+  }, [sectionState, isExiting]);
+  return ( 
     <>
-      {sectionState === "Inicio" && (
-        <div className={styles.container}>
+      {currentSection === "Inicio" && (
+        <div className={`${styles.container} ${animationClass}`}>
           <p className={` ${styles.description}`}>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum minima corrupti numquam consequuntur saepe impedit doloremque, voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia labore obcaecati ea ex rem.
-            Consectetur quisquam dicta impedit laborum! Voluptates repellendus soluta dignissimos atque perspiciatis delectus impedit illo consectetur obcaecati rem quo iste voluptate nesciunt excepturi magnam, quas quae explicabo eveniet. Nesciunt, repudiandae blanditiis?
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            minima corrupti numquam consequuntur saepe impedit doloremque,
+            voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia
+            labore obcaecati ea ex rem. Consectetur quisquam dicta impedit
+            laborum! Voluptates repellendus soluta dignissimos atque
+            perspiciatis delectus impedit illo consectetur obcaecati rem quo
+            iste voluptate nesciunt excepturi magnam, quas quae explicabo
+            eveniet. Nesciunt, repudiandae blanditiis?
           </p>
         </div>
       )}
-      {sectionState === "Proyectos" && (
-        <div className={styles.container}>
+      {currentSection === "Proyectos" && (
+        <div className={`${styles.container} ${animationClass}`}>
           <p className={` ${styles.description}`}>
-            Aorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum minima corrupti numquam consequuntur saepe impedit doloremque, voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia labore obcaecati ea ex rem.
-            Consectetur quisquam dictaA XD
+            Aorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
+            voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia
+            labore obcaecati ea ex rem. Consectetur quisquam dictaA XD
           </p>
         </div>
       )}
-      {sectionState === "Contacto" && (
-        <div className={styles.container}>
+      {currentSection === "Contacto" && (
+        <div className={`${styles.container} ${animationClass}`}>
           <p className={` ${styles.description}`}>
-            Aorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum minima corrupti numquam consequuntur saepe impedit doloremque, voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia labore obcaecati ea ex rem.
-            Consectetur  asd  asdas asd as das das da a q 2easd a sdas dquisquam dictaA XD
+            Aorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum
+            minima corrupti numquam consequuntur saepe impedit doloremque,
+            voluptas, eos ad inventore nostrum veritatis nulla enim. Mollitia
+            labore obcaecati ea ex rem. Consectetur asd asdas asd as das das da
+            a q 2easd a sdas dquisquam dictaA XD
           </p>
         </div>
       )}
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,29 +1,37 @@
-"use client"
-import { createContext, useState, useContext } from 'react';
+"use client";
+import { createContext, useState, useContext } from "react";
 
 const AppContext = createContext();
 
 export function AppProvider({ children }) {
-    const [sectionState, setSectionState] = useState('Inicio');
+  const [sectionState, setSectionState] = useState("Inicio");
+  const [isExiting, setIsExiting] = useState(false);
+  const changeSection = (newSection) => {
+    setIsExiting(true);
+    setTimeout(() => {
+      setSectionState(newSection);
+      setIsExiting(false);
+    }, 500);
+  };
 
-    const changeSection = (newSection) => {
-        setSectionState(newSection);
-    };
-
-    return (
-        <AppContext.Provider value={{
-            sectionState,
-            changeSection
-        }}>
-            {children}
-        </AppContext.Provider>
-    );
+  return (
+    <AppContext.Provider
+      value={{
+        sectionState,
+        changeSection,
+        isExiting,
+        setIsExiting,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 }
 
 export function useAppContext() {
-    const context = useContext(AppContext);
-    if (context === undefined) {
-        throw new Error('useAppContext debe usarse dentro de un AppProvider');
-    }
-    return context;
+  const context = useContext(AppContext);
+  if (context === undefined) {
+    throw new Error("useAppContext debe usarse dentro de un AppProvider");
+  }
+  return context;
 }
