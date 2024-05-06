3bf71e26ee258c5d863194a4fd9170f851531227
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-05-06 10:21:06 -0300

    install mui, add stickman, add pdf and add Tooltip

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -1,21 +1,31 @@
+"use client";
 import React from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { FaFilePdf } from "react-icons/fa6";
 import styles from "./SocialMedia.module.css";
+import { Tooltip } from "@mui/material";
 import Link from "next/link";
-
 export default function SocialMedia() {
   return (
     <div className={styles.social_media}>
-      <Link href={"https://github.com/lautaroAguilar"} target="_blank">
-        <FaGithub className={styles.shining} />
-      </Link>
-      <Link href={"https://www.linkedin.com/in/lautaro-aguilar-83a3a923b/"} target="_blank">
-        <FaLinkedin className={styles.shining} />
-      </Link>
-      <Link href={"#"} target="_blank">
-        <FaFilePdf className={styles.shining} />
-      </Link>
+      <Tooltip title={"Github"}>
+        <Link href={"https://github.com/lautaroAguilar"} target="_blank">
+          <FaGithub className={styles.shining} />
+        </Link>
+      </Tooltip>
+      <Tooltip title={"Linkedin"}>
+        <Link
+          href={"https://www.linkedin.com/in/lautaro-aguilar-83a3a923b/"}
+          target="_blank"
+        >
+          <FaLinkedin className={styles.shining} />
+        </Link>
+      </Tooltip>
+      <Tooltip title={"Descargar PDF"}>
+        <a href="/cv.pdf" download="cv">
+          <FaFilePdf className={styles.shining} />
+        </a>
+      </Tooltip>
     </div>
   );
 }
