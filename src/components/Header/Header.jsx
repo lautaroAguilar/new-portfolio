c31127a6c472984e33452f3dbdb643478d4639b5
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-05-13 11:51:22 -0300

    header adapted

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,33 @@
 "use client";
 import { useAppContext } from "@/context/appContext";
 import styles from "./Header.module.css";
-import { Bodoni_Moda} from "next/font/google";
+import { Bodoni_Moda } from "next/font/google";
 export const bodoni = Bodoni_Moda({
-  weight: ["400", "700", "900",],
+  weight: ["400", "700", "900"],
   subsets: ["latin"],
-  style: ["italic", "normal"]
+  style: ["italic", "normal"],
 });
 export default function Header() {
   const { changeSection } = useAppContext();
   return (
     <header className={styles.header}>
       <div className={styles.texts}>
-        <h1 className={`${bodoni.className} ${styles.title}`}>Lautaro Aguilar</h1>
+        <h1 className={`${bodoni.className} ${styles.title}`}>
+          Lautaro Aguilar
+        </h1>
         <p className={styles.description}>Desarrollador web</p>
       </div>
-        <ul className={styles.links}>
-          <li className={styles.link} onClick={() => changeSection("Inicio")}>
-            Inicio
-          </li>
-          <li
-            className={styles.link}
-            onClick={() => changeSection("Proyectos")}
-          >
-            Proyectos
-          </li>
-          <li className={styles.link} onClick={() => changeSection("Contacto")}>
-            Contacto
-          </li>
-        </ul>
+      <ul className={styles.links}>
+        <li className={styles.link} onClick={() => changeSection("Inicio")}>
+          Inicio
+        </li>
+        <li className={styles.link} onClick={() => changeSection("Proyectos")}>
+          Proyectos
+        </li>
+        <li className={styles.link} onClick={() => changeSection("Contacto")}>
+          Contacto
+        </li>
+      </ul>
     </header>
   );
 }
