082266bcbb047f69821a7d9654d09a53fc1021c4
Lautaro Aguilar <97558288+lautaroAguilar@users.noreply.github.com>
2024-10-17 00:01:12 -0300

    install framer-motion, add bear svg, Loading component created and imported into page, delete Stickman component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import styles from "./page.module.css";
 import Header from "../components/Header/Header";
 import BackgroundAnimation from "@/components/BackgroundAnimation/BackgroundAnimation";
@@ -6,17 +8,33 @@ import Section from "@/components/Section/Section";
 import SocialMedia from "@/components/SocialMedia/SocialMedia";
 import ImageModal from "@/components/ImageModal/ImageModal";
 import { useAppContext } from "@/context/appContext";
+import Loading from "@/components/Loading/Loading";
 export default function Home() {
   const { isOpen, images, closeModal } = useAppContext();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <main className={`${styles.main}`}>
-      <BackgroundAnimation />
-      <Header />
-      <section className={`${styles.section}`}>
-        <Section />
-      </section>
-      <SocialMedia />
-      {isOpen && <ImageModal images={images} onClose={closeModal} />}
-    </main>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <main className={`${styles.main}`}>
+          <BackgroundAnimation />
+          <Header />
+          <section className={`${styles.section}`}>
+            <Section />
+          </section>
+          <SocialMedia />
+          {isOpen && <ImageModal images={images} onClose={closeModal} />}
+        </main>
+      )}
+    </>
   );
 }
